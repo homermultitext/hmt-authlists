dd93e5444b64915d75bce19974772c5bd42bf316
Neel Smith <dnsmith.neel@gmail.com>
2022-02-04 13:37:59 -0500

    Move files

---
+++ xml-dingbats/dingbats.jl
@@ -0,0 +1,30 @@
+### A Pluto.jl notebook ###
+# v0.17.7
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ e8442bd2-85de-11ec-1946-03fc73aeb551
+md"""
+# HMT project dingbat viewer
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
+
+julia_version = "1.7.0"
+manifest_format = "2.0"
+
+[deps]
+"""
+
+# ╔═╡ Cell order:
+# ╟─e8442bd2-85de-11ec-1946-03fc73aeb551
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002
